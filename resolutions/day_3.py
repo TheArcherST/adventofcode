656b651bd0b8d8476687218bf83ee754f178dfe9
Shapovalov Mihail <67328068+TheArcherST@users.noreply.github.com>
2022-12-03 09:10:55 +0300

    Day 3
    I'm tried to find way to match groups in task_2. When i desperate and go to watch solutions, turned out that groups were given in right introduction order. So, I'm passed first task in 00:10:59, and second in 01:03:54.

---
+++ resolutions/day_3.py
@@ -0,0 +1,59 @@
+import itertools
+
+from fastaof import AdventOfCodePuzzle
+
+
+class Resolution(AdventOfCodePuzzle):
+    def task_1(self, data):
+        """Some task resolution
+
+        :input 1:
+            vJrwpWtwJgWrhcsFMMfFFhFp
+            jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+            PmmdzqPrVvPwwTWBwg
+            wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+            ttgJtRGJQctTZtZT
+            CrZsJsPPZsGzwwsLwLmpwMDw
+        :output 1:
+            157
+
+        """
+        sum_ = 0
+        for i in data.strip().split():
+            a, b = i[len(i) // 2:], i[:len(i) // 2]
+            lt = set(a) & set(b)
+            for i in lt:
+                if ord(i) in range(97, 123):
+                    sum_ += ord(i) - 96
+                else:
+                    sum_ += ord(i) - 38
+        return str(sum_)
+
+    def task_2(self, data):
+        """Some task resolution
+
+        :input 1:
+            vJrwpWtwJgWrhcsFMMfFFhFp
+            jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+            PmmdzqPrVvPwwTWBwg
+            wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+            ttgJtRGJQctTZtZT
+            CrZsJsPPZsGzwwsLwLmpwMDw
+        :output 1:
+            70
+
+        """
+
+        data = data.strip().split()
+        literals = ''
+        for i in range(0, len(data), 3):
+            a, b, c = data[i:i+3]
+            literals += (set(a) & set(b) & set(c)).pop()
+
+        sum_ = 0
+        for i in literals:
+            if ord(i) in range(97, 123):
+                sum_ += ord(i) - 96
+            else:
+                sum_ += ord(i) - 38
+        return str(sum_)
