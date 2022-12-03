cee66e0c8f780d72dcc212c28f2e96aba88eb2e2
Shapovalov Mihail <67328068+TheArcherST@users.noreply.github.com>
2022-12-03 12:39:17 +0300

    Change env load logic
    * Change env variable name from SESSION to ADVENT_OF_CODE_SESSION
    * Now, you can not pass data into `.env`, but just export ADVENT_OF_CODE_SESSION.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 import dotenv
 from rich.console import Console
 
@@ -6,12 +8,13 @@ from fastaof.structure import load_modules
 
 
 def main():
-    data = dotenv.dotenv_values()
+    dotenv.load_dotenv()
+
     console = Console()
 
     load_modules()
     latest = AdventOfCodePuzzle.get_latest()
-    executor = ResolutionExecutor(console, latest, data['SESSION'])
+    executor = ResolutionExecutor(console, latest, os.environ['ADVENT_OF_CODE_SESSION'])
     executor.run()
 
 
