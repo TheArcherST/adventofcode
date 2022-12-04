c20e5c805a86d9e09e3c6b184cbcec62c3be8155
Shapovalov Mihail <67328068+TheArcherST@users.noreply.github.com>
2022-12-04 08:19:11 +0300

    Day 4
    Added second task. I overwrite first by second, while solving it

--- 2022/day_4.py
+++ 2022/day_4.py
@@ -14,6 +14,35 @@ class Solution(AdventOfCodePuzzle):
             6-6,4-6
             2-6,4-8
         :output 1:
+            2
+
+        """
+
+        data = data.strip()
+        res = 0
+        for i in data.split('\n'):
+            a, b = i.split(',')
+            a1, a2 = a.split('-')
+            b1, b2 = b.split('-')
+            range1 = range(int(a1), int(a2))
+            range2 = range(int(b1), int(b2))
+            il = len(set(range1) & set(range2))
+            if il == len(range1) or il == len(range2):
+                res += 1
+        return str(res)
+
+    def task_2(self, data):
+
+        """Some task resolution
+
+        :input 1:
+            2-4,6-8
+            2-3,4-5
+            5-7,7-9
+            2-8,3-7
+            6-6,4-6
+            2-6,4-8
+        :output 1:
             4
 
         """
