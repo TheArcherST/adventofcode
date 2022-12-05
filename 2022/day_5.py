890b4acba69c17938ec51d671ff9b78a0395cac0
Shapovalov Mihail <67328068+TheArcherST@users.noreply.github.com>
2022-12-06 01:09:01 +0300

    Day 5 refactor
    Refactor code & add tests.

--- 2022/day_5.py
+++ 2022/day_5.py
@@ -1,5 +1,5 @@
 from fastaof import AdventOfCodePuzzle
-from pprint import pp
+
 
 class Solution(AdventOfCodePuzzle):
     def task_1(self, data):
@@ -7,40 +7,50 @@ class Solution(AdventOfCodePuzzle):
         """Some task solution
 
         :input 1:
-          ell
+            .. raw:: txt
 
-          ell
+                    [D]
+                [N] [C]
+                [Z] [M] [P]
+                 1   2   3
+
+                move 1 from 2 to 1
+                move 3 from 1 to 3
+                move 2 from 2 to 1
+                move 1 from 1 to 2
+
         :output 1:
-          CMZ
+            .. raw:: txt
+
+                CMZ
 
         """
         first, second = data.split("\n\n")
-        
-        lsts = [[] for _ in range(20)]
+
+        lsts = []
 
         lines = first.split('\n')
         for i in lines:
-          for nj, j in enumerate(i[1::4]):
-            if j.isnumeric():continue
-            if j == ' ':
-              continue
-            lsts[nj].append(j)
-        lsts=list(map(lambda x: list(reversed(x)),lsts))
-        
+            for nj, j in enumerate(i[1::4]):
+                if j.isnumeric() or j == ' ':
+                    continue
+                while len(lsts) <= nj:
+                    lsts.append([])
+                lsts[nj].append(j)
+        lsts = list(map(lambda x: list(reversed(x)), lsts))
+
         for i in second.split("\n"):
-            if len(i)<5:continue
+            if len(i) < 5: continue
             fromc, fromn, ton = map(int, i.split()[1::2])
-            sel=lsts[fromn-1]
-            lsts[ton-1] += reversed(sel[len(sel)-fromc:])
+            sel = lsts[fromn - 1]
+            lsts[ton - 1] += reversed(sel[len(sel) - fromc:])
             for _ in range(fromc):
-              lsts[fromn-1].pop()
+                lsts[fromn - 1].pop()
 
         res = ""
-      
+
         for i in lsts:
-          try:
-            res+=i[-1]
-          except: pass
+            res += i[-1]
 
         return res
 
@@ -49,39 +59,48 @@ class Solution(AdventOfCodePuzzle):
         """Some task solution
 
         :input 1:
-          ell
+            .. raw:: txt
 
-          ell
+                    [D]
+                [N] [C]
+                [Z] [M] [P]
+                 1   2   3
+
+                move 1 from 2 to 1
+                move 3 from 1 to 3
+                move 2 from 2 to 1
+                move 1 from 1 to 2
+
         :output 1:
-          CMZ
+            .. raw:: txt
+
+                MCD
 
         """
         first, second = data.split("\n\n")
-        
-        lsts = [[] for _ in range(20)]
+        lsts = []
 
         lines = first.split('\n')
         for i in lines:
-          for nj, j in enumerate(i[1::4]):
-            if j.isnumeric():continue
-            if j == ' ':
-              continue
-            lsts[nj].append(j)
-        lsts=list(map(lambda x: list(reversed(x)),lsts))
-        
+            for nj, j in enumerate(i[1::4]):
+                if j.isnumeric() or j == ' ':
+                    continue
+                while len(lsts) <= nj:
+                    lsts.append([])
+                lsts[nj].append(j)
+        lsts = list(map(lambda x: list(reversed(x)), lsts))
         for i in second.split("\n"):
-            if len(i)<5:continue
+            if len(i) < 5:
+                continue
             fromc, fromn, ton = map(int, i.split()[1::2])
-            sel=lsts[fromn-1]
-            lsts[ton-1] += sel[len(sel)-fromc:]
+            sel = lsts[fromn - 1]
+            lsts[ton - 1] += sel[len(sel) - fromc:]
             for _ in range(fromc):
-              lsts[fromn-1].pop()
+                lsts[fromn - 1].pop()
 
         res = ""
-      
+
         for i in lsts:
-          try:
-            res+=i[-1]
-          except: pass
+            res += i[-1]
 
         return res
