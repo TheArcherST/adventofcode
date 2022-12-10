ab44088443426f6f508a9d2855fa965ff9df3dac
Mihail Shapovalov <67328068+TheArcherST@users.noreply.github.com>
2022-12-10 11:01:25 +0000

    Day 10

---
+++ 2022/day_10.py
@@ -0,0 +1,380 @@
+from fastaof import AdventOfCodePuzzle
+import time
+
+class Solution(AdventOfCodePuzzle):
+    def task_1(self, data):
+
+        """Some task solution
+
+        :input 1:
+            addx 15
+            addx -11
+            addx 6
+            addx -3
+            addx 5
+            addx -1
+            addx -8
+            addx 13
+            addx 4
+            noop
+            addx -1
+            addx 5
+            addx -1
+            addx 5
+            addx -1
+            addx 5
+            addx -1
+            addx 5
+            addx -1
+            addx -35
+            addx 1
+            addx 24
+            addx -19
+            addx 1
+            addx 16
+            addx -11
+            noop
+            noop
+            addx 21
+            addx -15
+            noop
+            noop
+            addx -3
+            addx 9
+            addx 1
+            addx -3
+            addx 8
+            addx 1
+            addx 5
+            noop
+            noop
+            noop
+            noop
+            noop
+            addx -36
+            noop
+            addx 1
+            addx 7
+            noop
+            noop
+            noop
+            addx 2
+            addx 6
+            noop
+            noop
+            noop
+            noop
+            noop
+            addx 1
+            noop
+            noop
+            addx 7
+            addx 1
+            noop
+            addx -13
+            addx 13
+            addx 7
+            noop
+            addx 1
+            addx -33
+            noop
+            noop
+            noop
+            addx 2
+            noop
+            noop
+            noop
+            addx 8
+            noop
+            addx -1
+            addx 2
+            addx 1
+            noop
+            addx 17
+            addx -9
+            addx 1
+            addx 1
+            addx -3
+            addx 11
+            noop
+            noop
+            addx 1
+            noop
+            addx 1
+            noop
+            noop
+            addx -13
+            addx -19
+            addx 1
+            addx 3
+            addx 26
+            addx -30
+            addx 12
+            addx -1
+            addx 3
+            addx 1
+            noop
+            noop
+            noop
+            addx -9
+            addx 18
+            addx 1
+            addx 2
+            noop
+            noop
+            addx 9
+            noop
+            noop
+            noop
+            addx -1
+            addx 2
+            addx -37
+            addx 1
+            addx 3
+            noop
+            addx 15
+            addx -21
+            addx 22
+            addx -6
+            addx 1
+            noop
+            addx 2
+            addx 1
+            noop
+            addx -10
+            noop
+            noop
+            addx 20
+            addx 1
+            addx 2
+            addx 2
+            addx -6
+            addx -11
+            noop
+            noop
+            noop
+        :output 1:
+            13140
+
+        """
+        sum_ = 1
+
+        def is_signal(number: int):
+            return (number - 20) % 40 == 0
+        calln = 0
+        result = 0
+
+        def nextc():
+            nonlocal calln, result, sum_
+            calln+=1
+            
+            if is_signal(calln):
+                self.console.print(sum_, calln)
+                
+                result += (sum_) * calln
+            
+            
+        for i in data.strip().split('\n'):
+
+            if i.startswith('addx'):
+                number = int(i.split()[1])
+                nextc()
+                nextc()
+                self.console.print(number)
+                
+                sum_+=number
+                
+            elif i == "noop":
+                nextc()
+            else:
+                raise
+                
+
+        return result
+
+
+    def task_2(self, data):
+
+        """Some task solution
+
+        :input 1:
+            addx 15
+            addx -11
+            addx 6
+            addx -3
+            addx 5
+            addx -1
+            addx -8
+            addx 13
+            addx 4
+            noop
+            addx -1
+            addx 5
+            addx -1
+            addx 5
+            addx -1
+            addx 5
+            addx -1
+            addx 5
+            addx -1
+            addx -35
+            addx 1
+            addx 24
+            addx -19
+            addx 1
+            addx 16
+            addx -11
+            noop
+            noop
+            addx 21
+            addx -15
+            noop
+            noop
+            addx -3
+            addx 9
+            addx 1
+            addx -3
+            addx 8
+            addx 1
+            addx 5
+            noop
+            noop
+            noop
+            noop
+            noop
+            addx -36
+            noop
+            addx 1
+            addx 7
+            noop
+            noop
+            noop
+            addx 2
+            addx 6
+            noop
+            noop
+            noop
+            noop
+            noop
+            addx 1
+            noop
+            noop
+            addx 7
+            addx 1
+            noop
+            addx -13
+            addx 13
+            addx 7
+            noop
+            addx 1
+            addx -33
+            noop
+            noop
+            noop
+            addx 2
+            noop
+            noop
+            noop
+            addx 8
+            noop
+            addx -1
+            addx 2
+            addx 1
+            noop
+            addx 17
+            addx -9
+            addx 1
+            addx 1
+            addx -3
+            addx 11
+            noop
+            noop
+            addx 1
+            noop
+            addx 1
+            noop
+            noop
+            addx -13
+            addx -19
+            addx 1
+            addx 3
+            addx 26
+            addx -30
+            addx 12
+            addx -1
+            addx 3
+            addx 1
+            noop
+            noop
+            noop
+            addx -9
+            addx 18
+            addx 1
+            addx 2
+            noop
+            noop
+            addx 9
+            noop
+            noop
+            noop
+            addx -1
+            addx 2
+            addx -37
+            addx 1
+            addx 3
+            noop
+            addx 15
+            addx -21
+            addx 22
+            addx -6
+            addx 1
+            noop
+            addx 2
+            addx 1
+            noop
+            addx -10
+            noop
+            noop
+            addx 20
+            addx 1
+            addx 2
+            addx 2
+            addx -6
+            addx -11
+            noop
+            noop
+            noop
+        :output 1:
+            13140
+
+        """
+        x = 1
+
+        def is_next(number: int):
+            return number % 40
+        calln = 0
+        result = 0
+        crt = [''for _ in range(6)]
+        
+        def nextc():
+            nonlocal calln
+            calln+=1
+
+            current_crt = ((calln-1) // 40)
+            tilemap = '.'*(x-1) + '#'*3 + '.'*(40-3-(x-1))
+            crt[current_crt] += tilemap[calln-1-current_crt*40] + ''
+
+        for i in data.strip().split('\n'):
+            if i.startswith('addx'):
+                number = int(i.split()[1])
+                nextc()
+                nextc()
+                x+=number
+            elif i == "noop":
+                nextc()
+            else:
+                raise
+
+        print('\n'.join(crt))
+
+        return result
