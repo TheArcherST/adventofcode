a755310c08a0863a65c08b6cf08a5dbe0b8ab251
Shapovalov Mihail <67328068+TheArcherST@users.noreply.github.com>
2022-12-14 23:22:57 +0300

    Optimize 2022::14::1
    Apply the same optimisation to first part

--- 2022/day_14.py
+++ 2022/day_14.py
@@ -1,24 +1,9 @@
-import time
-
 from fastaoc import AdventOfCodePuzzle
 
-from enum import IntEnum
 from typing import Optional
 from itertools import islice
 
 from utils.coordinates import Coordinates
-
-
-class MoveOffset:
-    DOWN = Coordinates(1, 0)
-    LEFT = Coordinates(0, -1)
-    RIGHT = Coordinates(0, 1)
-
-
-class GridCell(IntEnum):
-    AIR = 0
-    SEND = 1
-    ROCK = 2
 
 
 SEND_SOURCE = (0, 500)
@@ -48,100 +33,43 @@ class Solution(AdventOfCodePuzzle):
 
         """
 
-        _SEND_SOURCE = Coordinates(0, 500)
-
-        # to normally use grid indexing, let's normalize `y` coordinates (set most left to zero)
-
-        rocks = []
-
-        y_offset = float('inf')
-
-        max_x = 0
-        max_y = 0
+        obtained = set()
 
         for i in data.strip().split('\n'):
             coordinates = [Coordinates(*map(int, reversed(j.split(',')))) for j in i.split(' -> ')]
             for start, end in zip(coordinates, islice(coordinates, 1, None)):
                 for x in abs_range(start.x, end.x, addition=1):
                     for y in abs_range(start.y, end.y, addition=1):
-                        y_offset = min(y_offset, y)
 
-                        max_x = max(max_x, x)
-                        max_y = max(max_y, y)
+                        obtained.add((x, y))
 
-                        coordinate = Coordinates(x, y)
-                        rocks.append(coordinate)
+        max_x = max(*(i[0] for i in obtained))
+        max_y = max(*(i[1] for i in obtained))
 
-        grid: list[list[GridCell]] = [
-            [GridCell.AIR for _ in range(max_y + 1 - y_offset)]
-            for _ in range(max_x + 1)
-        ]
-
-        # -   0  1  2 -> y (y is column)
-        #
-        # 0   0  0  0
-        # 1   0  1  0
-        # 2   2  2  2
-        # |
-        # v
-        # x (x is row)
-        #
-        # selecting element for point (x, y): grid[x][y]
-
-        for i in rocks:
-            grid[i.x][i.y - y_offset] = GridCell.ROCK
-
-        current_send_coord: Optional[Coordinates] = None
-        new_send = True
-
-        ox_range = range(0, max_x + 1)
-        oy_range = range(0, max_y - y_offset + 1)
+        current_send_coord = SEND_SOURCE
 
         result = 0
 
-        def render():
-            __r = ''
-            for _1 in grid:
-                for _2 in _1:
-                    if _2 is GridCell.AIR:
-                        l_ = '.'
-                    elif _2 is GridCell.SEND:
-                        l_ = 'o'
-                    else:
-                        l_ = '#'
-
-                    __r += l_ + ' '
-                __r += '\n'
-            __r += '======'*4
-            return __r
-
         while True:
-            if new_send:
-                current_send_coord = _SEND_SOURCE - (0, y_offset) + MoveOffset.DOWN
-                new_send = False
+            for x_offset, y_offset in ((1, 0), (1, -1), (1, 1)):
 
-            for i in ((1, 0), (1, -1), (1, 1)):
-                test_send_coord = current_send_coord + i
+                test_send_coord = (current_send_coord[0] + x_offset,
+                                   current_send_coord[1] + y_offset)
 
-                if (
-                    test_send_coord.x not in ox_range
-                    or test_send_coord.y not in oy_range
+                if not (
+                    (0 <= test_send_coord[0] <= max_x)
+                    and (0 <= test_send_coord[1] <= max_y)
                 ):
                     return str(result)
 
-                test_send_coord_val = grid[test_send_coord.x][test_send_coord.y]
-
-                if test_send_coord_val is GridCell.AIR:
+                if test_send_coord not in obtained:
                     current_send_coord = test_send_coord
                     break
-                elif test_send_coord_val in (GridCell.ROCK, GridCell.SEND):
-                    continue
-                else:
-                    raise RuntimeError()
+
             else:
                 result += 1
-                new_send = True
-                grid[current_send_coord.x][current_send_coord.y] = GridCell.SEND
+                obtained.add(current_send_coord)
+                current_send_coord = SEND_SOURCE
 
     def task_2(self, data):
 
